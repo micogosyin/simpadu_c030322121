236ec58abc621299fa780f2ab5faf5b121212769
PRASETIO LUKITO <frederichcomrade@gmail.com>
2024-01-01 17:05:03 +0800

    file uploads

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(15)->create();
+
+        User::create([
+            'name' => 'Mico Gosyin',
+            'email' => 'c030322121@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345'),
+        ]);
+    }
+}
---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; simpadu-c030322121 2023
+  </div>
